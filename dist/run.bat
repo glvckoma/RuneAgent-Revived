f257ef281a855f94455b5907085ffabd7e326d8a
Glockoma <winklernathan00@gmail.com>
2025-03-08 23:18:55 -0500

    no z

--- dist/run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"c:\program files\java\jdk-17\bin\java.exe" -noverify -cp "RuneAgent.jar;lib/bcel-6.3.jar;lib/rsyntax.jar;lib/nashorn-core-15.4.jar;client.jar" -javaagent:"RuneAgent.jar"=config.js -jar client.jar
-pause
